6a9979156ce8efbf33e70a51cd14acc76e8e2cd9
kharerishabh <rishabkhare41@gmail.com>
2023-02-04 21:15:04 +0530

    Project E-Commerce Task-6 new project

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Something went wrong...Retrying");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((moviesData) => {
+        return {
+          id: moviesData.episode_id,
+          title: moviesData.title,
+          openingText: moviesData.opening_crawl,
+          releaseDate: moviesData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
+    let content = <p>Found no Movies</p>;
+    if(movies.length > 0) {
+      content = <MoviesList movies={movies} />
+    }
+    if(error) {
+      content = <p>{error}</p>
+    }
+    if(isLoading) {
+      content = <p>Loading...</p>
+    }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {content}
+        {error && <button>Stop</button>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
