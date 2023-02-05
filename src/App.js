cebe47ee1242fbb9f456a284086b2d8799ae1de1
kharerishabh <rishabkhare41@gmail.com>
2023-02-05 18:26:00 +0530

    Project E-Commerce Task-10

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -36,14 +37,18 @@ function App() {
     }
 
     setIsLoading(false);
-  }, [])
+  }, []);
 
   useEffect(() => {
-    fetchMoviesHandler()
+    fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   const stopFetching = () => {
-    console.log('Stopfetching')
+    console.log("Stopfetching");
     clearInterval(stop);
   };
 
@@ -60,6 +65,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
