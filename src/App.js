caf57e808e2699b43496eed492bb82d1ad8a7f93
kharerishabh <rishabkhare41@gmail.com>
2023-02-05 22:05:46 +0530

    Project E-Commerce Task-11

--- src/App.js
+++ src/App.js
@@ -14,24 +14,31 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-ecommerce-module-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong...Retrying");
       }
       const data = await response.json();
-      const transformedMovies = data.results.map((moviesData) => {
-        return {
-          id: moviesData.episode_id,
-          title: moviesData.title,
-          openingText: moviesData.opening_crawl,
-          releaseDate: moviesData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
       const cleanInterval = setInterval(async () => {
-        await fetch("https://swapi.dev/api/films/");
+        await fetch(
+          "https://react-ecommerce-module-default-rtdb.firebaseio.com/movies.json"
+        );
       }, 5000);
       setStop(cleanInterval);
     }
@@ -43,8 +50,19 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-ecommerce-module-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application.json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   const stopFetching = () => {
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,21 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+  async function deletMovieHandler () {
+    const response = await fetch(`https://react-ecommerce-module-default-rtdb.firebaseio.com/movies/${props.id}.json`, {
+      method: "DELETE"
+    })
+    if(!response.ok){
+        console.log('Something Went Wrong')
+    }
+    console.log("deleted")
+  }
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button onClick={deletMovieHandler} className={classes.button}>Delete Movie</button>
     </li>
   );
 };
